9b829223efadfeaf71b42f62b2f6607253802221
your name <shirigilboa@shiris-MacBook-Pro-2.local>
2023-02-18 13:31:46 +0200

    run analysis from program

--- nba_feed_project.py
+++ nba_feed_project.py
@@ -1,14 +1,16 @@
 import sys
+import os
 import papermill as pm
-
 from ingestion_pipeline import IngestionPipeLineManager
 from sql_db_manager import get_sql_con
-sys.path.append("./data_analysis")
+
 DEFAULT_DB_NAME = "NBA_DATA_BIG_DATA_PROJECT.db"
+analysis_path = "data_analysis"
 ANALYSIS_JUPYTER_NOTEBOOK = "Big_data_Final_project_analysis.ipynb"
 db_connection = None
 pipeline_manager = None
-print(sys.path)
+
+
 def is_valid_choice(operation: str):
     return operation.isdigit() and int(operation) in range(len(operations))
 
@@ -39,11 +41,20 @@ def build_update_db():
         pipeline_manager.update_db_connection(db_connection)
     pipeline_manager.run()
 
+
 def run_jupyter_notebook():
-    pm.execute_notebook(ANALYSIS_JUPYTER_NOTEBOOK, "results_{}".format(ANALYSIS_JUPYTER_NOTEBOOK))
+    print("Going to run Jupyer notebook {}".format(os.path.join(analysis_path, ANALYSIS_JUPYTER_NOTEBOOK)))
+    print("Output notebook will be in {}".format(os.path.join(analysis_path, "results_{}".format(ANALYSIS_JUPYTER_NOTEBOOK))))
+    print("It might take a while to run all analysis..")
+    pm.execute_notebook(input_path=os.path.join(analysis_path, ANALYSIS_JUPYTER_NOTEBOOK),
+                        output_path=os.path.join(analysis_path, "results_{}".format(ANALYSIS_JUPYTER_NOTEBOOK)),
+                        cwd=os.path.join(os.getcwd(), analysis_path), stdout_file=sys.stdout)
+
+
 def terminate_program():
     print("That's it for now, thank you !")
     exit(1)
+
 
 operations = {"Create New DB from scratch": create_new_db,
               "Update Your DB from NBA feed": build_update_db,
@@ -55,7 +66,7 @@ while True:
     print("When do you want to do ?")
     for operation in enumerate(operations.keys()):
         print(operation)
-    oper = input("Please choose valid option {} \n".format(range(len(operations) -1)))
+    oper = input("Please choose valid option {} \n".format(range(len(operations) - 1)))
     while not is_valid_choice(oper):
         print("{} is not good choice Please choose valid option (0-{})".format(oper, len(operations) - 1))
         oper = input("So.. What do like to do ? \n")
